3e499114fd4a838490f4d8ae4bc6067ee7069cc6
Loic WEBER <loic.weber@etu.univ-nantes.fr>
2024-01-12 10:01:11 +0000

    Le giga polywriter.py

---
+++ polywriter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from polysolver import *
+import sys
+
+
+def Writer(challenge: str, method: str) -> None:
+
+    if method == "naive_loic":
+        Solution = naive_approach_loic(challenge)
+    if method == "naive_AmeTheo":
+        Solution = naive_approach_loic(challenge)
+
+    # basename the challenge name and add .out at the end.
+    output_name = "solutions/" + \
+        str(str(challenge.split("/")[-1]).split(".")[0])+".out"
+
+    with open(output_name, 'w') as f:
+        # Write the number of line at the begining.
+        f.write(str(len(Solution)) + '\n')
+        for line in Solution:
+            f.write(line + '\n')
+
+    print("Done Writing to : "+str(output_name))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage: python polywriter.py naive_loic|naive_AmeTheo challenges/ma_map.in")
+    else:
+        Writer(sys.argv[2], sys.argv[1])
