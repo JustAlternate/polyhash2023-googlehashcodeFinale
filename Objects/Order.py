4ba9ad3992a412923bd8d96bfc63b71240df2128
amedeo calcagni <amedeo.calcagni@etu.univ-nantes.fr>
2024-01-17 18:20:54 +0100

    add a id on objects

--- Objects/Order.py
+++ Objects/Order.py
@@ -1,7 +1,8 @@
 class Order:
     def __init__(
-        self, destination: tuple[int, int], nb_products: int, products_qty: list[int]
+            self, id: int, destination: tuple[int, int], nb_products: int, products_qty: list[int]
     ):
+        self.id: int
         self.destination: tuple[int, int] = destination
         self.nb_products: int = nb_products
         self.products_qty: list[int] = products_qty
