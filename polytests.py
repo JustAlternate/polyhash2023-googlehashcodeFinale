d554f1a8cc83a1a45e849eb8aa08408314a0f415
amedeo calcagni <amedeo.calcagni@etu.univ-nantes.fr>
2024-01-17 18:25:04 +0100

    add a id on objects

--- polytests.py
+++ polytests.py
@@ -10,13 +10,13 @@ def printG(txt): print("\033[92m {}\033[00m" .format(txt))
 
 
 def test_Drone():
-    D = Drone((0, 0), [1, 1, 1])
+    D = Drone(0, (0, 0), [1, 1, 1])
     assert (D.calc_dist((0, 0)) == 0)
 
-    D = Drone((0, 0), [1, 1, 1])
+    D = Drone(0, (0, 0), [1, 1, 1])
     assert (D.calc_dist((2, 2)) == 3)
 
-    D = Drone((0, 0), [1, 1, 1])
+    D = Drone(0, (0, 0), [1, 1, 1])
     assert (D.calc_dist((0, 3)) == 3)
 
     printG("Drone tests COMPLETED")
